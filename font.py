989d675ebdfae1bce968271decabfd6fd4eec8a9
cypok is not CbIpok <dmitrienkomy@gmail.com>
2019-04-11 20:11:27 +0700

    print font
    python code for lesson

--- font.py
+++ font.py
@@ -3,6 +3,13 @@ from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 import numpy as np
+import block
+from mcpi.minecraft import Minecraft
+
+
+mc = Minecraft.create()
+start_x = start_y = start_z = end_x = end_y = end_z = 0
+
 
 def char_to_pixels(text, path='res/font.ttf', fontsize=20):
     font = ImageFont.truetype(path, fontsize)
@@ -17,3 +24,13 @@ def char_to_pixels(text, path='res/font.ttf', fontsize=20):
     return arr
 
 
+def print_text(pos_x, pos_y, pos_z, text, fontsize=20):
+    global start_x, start_y, start_z, end_x, end_y, end_z
+    mc.setBlocks(start_x, start_y, start_z, end_x, end_y, end_z, 0)
+    arr = char_to_pixels(text, fontsize=fontsize)
+    mc.setBlocks(pos_x, pos_y, pos_z, pos_x + len(arr[0]), pos_y + len(arr) + 1, pos_z, block.WOOL)
+    for y in range(len(arr)):
+        for x in range(len(arr[y])):
+            if arr[y][x] == 1:
+                mc.setBlock(pos_x + len(arr[y]) - x, pos_y + len(arr) - y, pos_z, arr[y][x])
+    start_x, start_y, start_z, end_x, end_y, end_z = (pos_x, pos_y, pos_z, pos_x + len(arr[0]), pos_y + len(arr) + 1, pos_z)
